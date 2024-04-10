f13a71f56921a49ea61b07f0762b87b409aac8fc
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-10 20:46:34 +0530

    login logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+'use client'
+
 import Link from 'next/link';
 import Image from 'next/image'
+import { signIn, useSession, signOut } from 'next-auth/react';
+
+
+
 export default function Header() {
+    const { data: session } = useSession()
+    console.log(session);
     return (
         <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
             <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -16,9 +24,17 @@ export default function Header() {
                     className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]'
                 />
 
-                <button className='font-semibold text-sm text-blue-500'>
-                    Log In
-                </button>
+
+                {
+                    session ? (
+                        <img src={session.user.image} alt={session.user.name} className='h-10 w-10 rounded-full object-cover cursor-pointer' onClick={()=>signOut()}/>
+                    ) : (
+                        <button onClick={() => signIn()} className='font-semibold text-sm text-blue-500'>
+                            Log In
+                        </button>
+                    )
+                }
+
             </div>
         </div>
     )
