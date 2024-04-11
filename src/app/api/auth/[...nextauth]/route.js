b30c911fde70a939ceb9b7168fcda235ca1d8c3d
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-11 12:47:37 +0530

    upload images to firebase fuctionality

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,6 +11,14 @@ const handler = NextAuth(
           }),
           // ...add more providers here
         ],
+
+        callbacks:{
+          async session({session,token}){
+            session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase()
+            session.user.uid = token.sub;
+            return session
+          }
+        }
       }
 )
 
