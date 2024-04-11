59b3b0b857bd2029eb2c1486f5166212bf86fb42
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-11 13:47:06 +0530

    feed section responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import Feed from "@/components/Feed";
+
 export default function Home() {
   return (
-    <div>page</div>
+    <>
+    <Feed />
+    </>
   )
 }
---
+++ src/components/MiniProfile.jsx
@@ -0,0 +1,6 @@
+
+export default function MiniProfile() {
+  return (
+    <div>MiniProfile</div>
+  )
+}
